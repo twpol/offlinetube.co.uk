40e4a765355cbbf2ca335b9d404c123cb79674da
James Ross <git@james-ross.co.uk>
2016-01-01 16:44:59 +0000

    Improve 2-segment routing by excluding direct routes at either end and not duplicating completely distinct routes

--- src/js/find-path.js
+++ src/js/find-path.js
@@ -1,6 +1,13 @@
 define([
 	'library/lodash/lodash'
 ], function (_) {
+	// Returns:
+	//   array of paths
+	//     array of segments
+	//       station name
+	//       array of routes
+	//       station name
+	//       ...
 	return function (network, station1, station2) {
 		console.log('Finding path from %s to %s', station1.name, station2.name);
 		console.time('find-path');
@@ -28,37 +35,58 @@ define([
 		//   is connected via a NON-direct route (i.e. a route not found above).
 		var indirect1 = _(network.stations)
 			.filter(function (station) {
-				return station !== station1 && station !== station2 &&
-					(_.intersection(station.routes, station1IndirectRoutes).length && _.intersection(station.routes, station2.routes).length ||
-					_.intersection(station.routes, station2IndirectRoutes).length && _.intersection(station.routes, station1.routes).length);
+				return station !== station1 && station !== station2 && (
+					_.intersection(station.routes, station1IndirectRoutes).length && _.intersection(station.routes, station2.routes).length ||
+					_.intersection(station.routes, station2IndirectRoutes).length && _.intersection(station.routes, station1.routes).length
+				);
 			})
 			.map(function (interchange) {
 				// Construct the indirect data: start, routes, interchange, routes, end.
+				// Include all routes
+				//   - start->intersection (except through end)
+				//   - intersection->end (except through start)
 				// Grouping by line so we split up the alternatives better.
 				// TODO: This should be based on platform/station stops or something?
 				var segment1Routes = _(station1.routes)
 					.intersection(interchange.routes)
+					.difference(station2.routes)
 					.groupBy(routeLine)
 					.values()
 					.value();
 				var segment2Routes = _(station2.routes)
 					.intersection(interchange.routes)
+					.difference(station1.routes)
 					.groupBy(routeLine)
 					.values()
 					.value();
 				return _.map(segment1Routes, function (segment1) {
 					return _.map(segment2Routes, function (segment2) {
-						return [
+						var overlap = _.intersection(segment1, segment2).length > 0;
+						if (overlap) {
+						return [[
+							station1.key,
+							segment1,
+							interchange.key,
+							_.difference(segment2, segment1),
+							station2.key
+						], [
+							station1.key,
+							_.difference(segment1, segment2),
+							interchange.key,
+							segment2,
+							station2.key
+						]];
+						}
+						return [[
 							station1.key,
 							segment1,
 							interchange.key,
 							segment2,
 							station2.key
-						];
+						]];
 					});
 				});
-			}).flatten().flatten().filter(function (indirectRoute) {
-				console.log(indirectRoute);
+			}).flatten().flatten().flatten().filter(function (indirectRoute) {
 				// Filter out any calculated interchanges where the possible routes are empty (due to being the same on both legs).
 				return indirectRoute[1].length > 0 && indirectRoute[3].length > 0;
 			})
